b1c8473d6dea80fbf31a3f359284988ac40c74f0
Sara Magdits <saramagdits@gmail.com>
2019-05-28 13:54:57 -0400

    Fetches CDC data, uses it to create a new chart. Currently just uses the title, need to explore the data more to better display it.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
+// Select canvas
 const ctx = document.getElementById('myChart').getContext('2d');
-const chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: 'line',
 
-  // The data for our dataset
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 255, 255, 0)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
+// Fetch JSON data
+// https://data.cdc.gov/api/views/bi63-dtpu/rows.json?accessType=DOWNLOAD
+fetch('https://data.cdc.gov/api/views/bi63-dtpu/rows.json?accessType=DOWNLOAD')
+  .then(res => res.json())
+  .then(json => makeChart(json));
+  // .then(json => JSON.stringify(json));
+
+
+// Create chart using response data
+const makeChart = (data) => {
+  const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: data.meta.view.name,
+        backgroundColor: 'rgb(255, 255, 255, 0)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45]
+      },
+        {
+          label: 'My Second dataset',
+          backgroundColor: 'rgb(255, 255, 255, 0)',
+          borderColor: 'rgb(0, 99, 255)',
+          data: [10, 2, 4, 2, 23, 5, 18]
+        }]
     },
-      {
-      label: 'My Second dataset',
-      backgroundColor: 'rgb(255, 255, 255, 0)',
-      borderColor: 'rgb(0, 99, 255)',
-      data: [10, 2, 4, 2, 23, 5, 18]
-    }]
-  },
 
-  // Configuration options go here
-  options: {}
-});
+    // Configuration options go here
+    options: {}
+  });
+};
+
